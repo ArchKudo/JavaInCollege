869383bb009e87a381b820160e33394e32b13078
ArchKudo <vgd.vyas17@gmail.com>
2016-07-31 23:39:23 +0530

    Added Practical No. 001

---
+++ aCertainLie.java
@@ -0,0 +1,11 @@
+/**
+ * @brief Program No. 001.
+ * WAJP to print a sentence.
+ * @note Practical No. 001.
+ * @author ArchKudo
+ */
+class aCertainLie {
+	public static void main(String args[]) {
+		System.out.println("Java is better than C.");
+	}
+}
