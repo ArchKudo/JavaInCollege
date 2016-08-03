8c0868e0e478fdb8acb45d5dbd9f239ced52e74b
ArchKudo <vgd.vyas17@gmail.com>
2016-08-04 01:03:42 +0530

    Fixed error

--- getSquareroot.java
+++ getSquareroot.java
@@ -9,6 +9,6 @@ class getSquareroot {
 	public static void main(String args[]) {
 		double x = 25;
 		double y = Math.sqrt(x);
-		System.out.println("The squareroot of " + x " is " + y);	
+		System.out.println("The squareroot of " + x + " is " + y);	
 	}
 }
