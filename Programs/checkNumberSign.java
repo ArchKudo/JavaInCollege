00bc4665660f2b473e981b40bc7c3e8f978c8775
ArchKudo <vgd.vyas17@gmail.com>
2016-08-06 14:01:14 +0530

    Added checkNumberSign.java

---
+++ Programs/checkNumberSign.java
@@ -0,0 +1,18 @@
+/**
+ * @brief Write a Java program to check whether a number is +ve, -ve or 0.
+ * @note Practical NO. 001
+ * @author ArchKudo
+ */
+class checkNumberSign {
+	public static void main(String args[]) {
+		String numberString = args[0];
+		int number = Integer.parseInt(numberString); /* Convert String to Integer using parseInt() */
+		if (number > 0) {
+			System.out.println("The given number " + number + " is positive.");
+		} else if (number < 0) {
+			System.out.println("The given number " + number + " is negative.");
+		} else {
+			System.out.println("The given number " + number + " is zero.");
+		}
+	}
+}
